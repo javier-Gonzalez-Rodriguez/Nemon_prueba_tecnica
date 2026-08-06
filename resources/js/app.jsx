b3cef121701a2c539d2bc91e8f299a71452dd4dc
JAVIER\javie <javiergr1234@gmail.com>
2026-08-06 09:23:36 +0200

    actualizadas dependencias de vite.config.js con prueba de funcionamiento visual al ejecutar el proyecto

---
+++ resources/js/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+    return (
+        <h1>¡React funciona dentro de Laravel!</h1>
+    );
+}
+
+ReactDOM.createRoot(document.getElementById('app')).render(
+    <App />
+);
