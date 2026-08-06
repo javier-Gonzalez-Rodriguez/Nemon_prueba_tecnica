a30a9270c6968f04e80eb63391c795622de7fb2f
JAVIER\javie <javiergr1234@gmail.com>
2026-08-06 13:00:30 +0200

    funcionalidad base e interfaz de calculadora de precios indexados preparada

--- app/Http/Controllers/Api/CalculateController.php
+++ app/Http/Controllers/Api/CalculateController.php
@@ -1,9 +1,16 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
+use Throwable;
+use Exception;
+
+use App\Models\Consumptions;
+use App\Models\Prices;
 
 class CalculateController extends Controller
 {
@@ -15,5 +22,38 @@ class CalculateController extends Controller
      */
     public function calculate(Request $request){
 
+        try{
+            $expression = new ExpressionLanguage();
+            $price_indexed = null;
+
+            try{
+                if($request->start_date == null || $request->end_date == null){
+                    throw new Exception('Invalid date');
+                }
+                /*if(...){
+                    return response()->json(['Error' => 'Dates range not valid'],404);
+                }*/
+
+                //resultado de aplicar la formula
+                $price_indexed = $expression->evaluate($request->formula, [
+                    //'salario' => 2000,
+                    //'bono' => 500,
+                ]);
+            }catch(Exception $e){
+                Log::error('Error al evaluar expresion', [
+                    'mensaje' => $e->getMessage(),
+                    'archivo' => $e->getFile(),
+                    'linea' => $e->getLine(),
+                    'trace' => $e->getTraceAsString(),
+                ]);
+
+                return response()->json(['Error' => 'Invalid data'], 400);
+            }
+
+
+            return response()->json(['price_indexed' => $price_indexed], 200);
+        }catch(Throwable $e){
+            return response()->json(['Error' => 'Unexpected error'], 500);
+        }
     }
 }
--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -3,10 +3,118 @@ import '../css/app.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import { useState } from 'react';
+import axios from 'axios';
+
 
 function App() {
+    //guarda los resultados de las operaciones previsas del usuario
+    const [results, setResults] = useState([]);
+
+    //contiene los datos del formulario
+    const [form, setForm] = useState({
+        start_date: '',
+        end_date: '',
+        formula: '',
+    });
+
+    //si el formulario contiene un error entonces error = true y cambia el color de los titulos a rojo
+    const [formError, setError] = useState(false);
+
+    /**
+     * actualiza los datos del formulario cuando el usuario realiza una modificación
+     * @param {Event} e 
+     */
+    const handleChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+
+    /**
+     * llama a la api para recuperar el resultado de la operación
+     * @param {Event} e evento que lanza la llamada a la api para obtener el resultado de la operación
+     */
+    const calculate = async (e) => {
+        e.preventDefault();
+        try{
+            const request = await axios.post('/api/calculate', form);
+
+            setResults([
+                {
+                    start_date: form.start_date,
+                    end_date: form.end_date,
+                    formula: form.formula,
+                    result: request.data.price_indexed,
+                },
+                ...results,
+            ]);
+            
+            setForm({
+                start_date: '',
+                end_date: '',
+                formula: ''
+            });
+
+        }catch(error){
+            console.error(error);
+            console.error(error.status);
+
+            if(error.status == 400){
+                setError(true);
+            }
+        }
+    };
+
     return (
-        <h1>¡React funciona dentro de Laravel!</h1>
+        <div className="calculator-body">
+            <div className="results-viewer">
+                {results.map((result, index) => (
+                            <div className="preview-item" key={index}>
+                                <span>{result.start_date} - {result.end_date}</span>
+                                <span>{result.formula} = <b>{result.result}</b></span>
+                            </div>
+                        )
+                    )
+                }
+            </div>
+
+
+            <div className="calculator-card-container">
+                <form onSubmit={calculate} className="calculator-card">
+                    <div className="formula-column-container" id="card-preview">
+                        <div><span className={formError ? 'error-title' : ''}>Formula</span>*</div>
+                        <input type="text" name="formula" id="form-formula" onChange={handleChange} value={form.formula} />
+                    </div>
+
+                    <div className="row-container">
+                        <div className="date-input-column">
+                            <div><span className={formError ? 'error-title' : ''}>Start date</span>*</div>
+                            <input type="date" name="start_date" id="form-start-date" onChange={handleChange} max={(form.end_date) || undefined} value={form.start_date} />
+                        </div>
+                        <div className="date-input-column">
+                            <div><span className={formError ? 'error-title' : ''}>End date</span>*</div>
+                            <input type="date" name="end_date" id="form-end-date" onChange={handleChange} min={(form.start_date) || undefined} value={form.end_date} />
+                        </div>
+                    </div>
+
+                    <button type='submit'>CALCULATE</button>
+
+                    <div className="operator-legend">
+                        <span>Operator legend</span>
+                        <div className='valid-operator-container'>
+                            <span className="legend">+    addition</span>
+                            <span className="legend">-    subtraction</span>
+                            <span className="legend">*    multiplication</span>
+                            <span className="legend">/    division</span>
+                            <span className="legend">%    mod (remainder)</span>
+                            <span className="legend">**   exponentiation</span>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
