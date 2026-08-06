b7c5fd91ee273828e3beecbde6c781a8d541e4bc
JAVIER\javie <javiergr1234@gmail.com>
2026-08-06 09:12:45 +0200

    creacion del proyecto, instalación de React y ceración de las migraciones para la BD

---
+++ database/migrations/2026_08_06_065325_create_consumptions_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consumptions', function (Blueprint $table) {
+            $table->id();
+            $table->date('date')->unique();
+            //Consumo de energía en kWh por cada hora del día
+            $table->double('h1_h25');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consumptions');
+    }
+};
